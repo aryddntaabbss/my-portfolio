125615a5fa9c513bce1ed8ba68bb5207e688a359
aryaddinata <aryaddinataabbas017@gmail.com>
2024-11-10 05:13:26 +0900

    add PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig( {
+  plugins: [
+    react(),
+    VitePWA( {
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My Portfolio',
+        short_name: 'Portfolio',
+        description: 'Portfolio of Agil Aryaddinata Abbas, Web Developer and Product Designer',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/logo 192x192.ico',
+            sizes: '192x192',
+            type: 'image/x-icon',
+          },
+          {
+            src: '/logo 512x512.ico',
+            sizes: '512x512',
+            type: 'image/x-icon',
+          },
+        ],
+      },
+    } ),
+  ],
+} );
