67ba49c9b37f1918d3416f375f1d2a41214112a9
aryaddinata <aryaddinataabbas017@gmail.com>
2024-11-10 04:33:09 +0900

    Fix Bug

--- src/components/ScrollTopButton.jsx
+++ src/components/ScrollTopButton.jsx
@@ -1,31 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-function ScrollTopButton ()
-{
-    const [ isVisible, setIsVisible ] = useState( false );
+function ScrollTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
 
-    useEffect( () =>
-    {
-        const toggleVisibility = () =>
-        {
-            setIsVisible( window.scrollY > 200 );
-        };
-        window.addEventListener( 'scroll', toggleVisibility );
-        return () => window.removeEventListener( 'scroll', toggleVisibility );
-    }, [] );
-
-    const scrollToTop = () =>
-    {
-        window.scrollTo( { top: 0, behavior: 'smooth' } );
+  useEffect(() => {
+    const toggleVisibility = () => {
+      setIsVisible(window.scrollY > 200);
     };
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
 
-    return (
-        isVisible && (
-            <button onClick={ scrollToTop } className="fixed bottom-14 right-10 bg-indigo-500 text-white p-3 rounded-full">
-                ↑
-            </button>
-        )
-    );
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    isVisible && (
+      <button
+        onClick={scrollToTop}
+        className="fixed bottom-14 right-10 bg-indigo-500 text-white p-3 rounded-full"
+      >
+        ↑
+      </button>
+    )
+  );
 }
 
 export default ScrollTopButton;
